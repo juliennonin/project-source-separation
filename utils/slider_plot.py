f47e0570659621d2a90ea9faccb29ee3ef5a43b8
Maun <julien.nonin@sfr.fr>
2019-11-28 21:55:05 +0100

    Admm with M=identity matrix

--- utils/slider_plot.py
+++ utils/slider_plot.py
@@ -9,15 +9,16 @@ class SliderPlot():
         pos = ax.get_position()
         rect = kwargs.get('rect', [pos.x0, pos.y0-0.03, pos.width, 0.03])
         slider_name = kwargs.get('legend', None)
+        valinit = kwargs.get('valinit', 0)
 
         self.I = I
         N, pixels = self.I.shape
         self.size = int(pixels**(0.5))
-        self.im = ax.imshow(I[0].reshape((self.size, -1)).T, cmap='gray')
+        self.im = ax.imshow(I[valinit].reshape((self.size, -1)).T, cmap='gray')
         ax.set_title(title)
         
         ax_slider = plt.axes(rect)
-        self.slider = Slider(ax_slider, slider_name, 0, N-1, valinit=0, valstep=1, valfmt="%.f")
+        self.slider = Slider(ax_slider, slider_name, 0, N-1, valinit=valinit, valstep=1, valfmt="%.f")
         self.slider.on_changed(self.update_img)
     
     def update_img(self, val):
