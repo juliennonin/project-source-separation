039df8a9e9261851d9fbc647c4e4926d07e730f9
Maun <julien.nonin@sfr.fr>
2020-03-05 06:43:49 +0100

    Gaussian kernel in pidal-tv

--- main_pidal.py
+++ main_pidal.py
@@ -6,12 +6,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import src.pidal as pidal
 
+from scipy.ndimage import gaussian_filter
+poisson = np.vectorize(np.random.poisson)
 # %%
-COEFF = 100
+COEFF = 3000
 X = plt.imread("data/img/cameraman_crop.png")[:,:,0]
 X *= COEFF
-poisson = np.vectorize(np.random.poisson)
-Y = poisson(X)
+
+# # blurred with Gaussian kernel
+Nx, Ny = X.shape
+# b = 5
+# Nx, Ny = Nx+2*b, Ny+2*b
+# Xtilde = np.zeros((Nx, Ny))
+# Xtilde[b:-b, b:-b] = X
+Xx, Yy = np.meshgrid(np.arange(Nx)-Nx/2, np.arange(Ny)-Ny/2)
+h = 1/(2*np.pi) * np.exp(-0.5*(Xx**2 + Yy**2))
+D = np.fft.fft2(h)
+Y = pidal.K(np.fft.fft2(h), X)
+# Y = gaussian_filter(X, 1)
+
+Y = poisson(Y)
 # %%
 plt.figure(figsize=(6, 8))
 plt.subplot(121)
@@ -20,7 +34,9 @@ plt.subplot(122)
 plt.imshow(Y, cmap='gray')
 
 # %%
-Z, maes, isnrs = pidal.pidal_tv(Y, np.eye(Y.shape[0]), X, 0.1)
+alpha = 0.008
+rho = 60 * alpha / np.max(X)
+Z, maes, isnrs = pidal.pidal_tv(Y, D, X, rho, alpha)
 
 # %%
 plt.figure(figsize=(10, 4))
@@ -31,11 +47,13 @@ plt.imshow(Y, cmap='gray')
 plt.subplot(133)
 plt.imshow(Z, cmap="gray")
 
+
 plt.figure(figsize=(8, 4))
 plt.subplot(121)
 plt.plot(maes)
 plt.xlabel('iterations')
 plt.ylabel('MAE')
+
 plt.subplot(122)
 plt.plot(isnrs)
 plt.xlabel('iterations')
--- src/pidal.py
+++ src/pidal.py
@@ -3,8 +3,15 @@ import numpy as np
 from IPython.display import clear_output
 from src.fbpd import total_variation, primal_dual_TV
 
+def K(D, X):
+    """KX"""
+    return np.abs(np.fft.fftshift(np.fft.ifft2(np.fft.fft2(X)*D)))
+    
+def KT(D, X):
+    """K^TX"""
+    return np.abs(np.fft.fftshift(np.fft.ifft2(np.fft.fft2(X)*D)))
 
-def pidal_tv(Y, K, X, rho, alpha=0.008, max_iter=200):
+def pidal_tv(Y, D, X, rho, alpha=0.008, max_iter=200):
     maes = []
     isnrs = []
     n = Y.shape[0]
@@ -19,18 +26,19 @@ def pidal_tv(Y, K, X, rho, alpha=0.008, max_iter=200):
     D2 = np.zeros(Y.shape)
     D3 = np.zeros(Y.shape)
 
-    I = np.eye(K.shape[1])
-    C = np.linalg.inv(2*I + K.T @ K)
+    def C(D, X):
+        """(K^* K + 2I)^{-1}X"""
+        return np.abs(np.fft.ifft2(np.fft.fft2(X) / (np.abs(D)**2 + 2)))
 
     for _ in range(max_iter):
         Zeta1 = U1 + D1
         Zeta2 = U2 + D2
         Zeta3 = U3 + D3
-        Gamma = K.T @ Zeta1 + Zeta2 + Zeta3
-        Z = C @ Gamma
+        Gamma = KT(D, Zeta1) + Zeta2 + Zeta3
+        Z = C(D, Gamma)
 
         # Splitting variables update
-        Nu = K @ Z - D1 - 1/rho
+        Nu = K(D, Z) - D1 - 1/rho
         U1 = 0.5 * (Nu + np.sqrt(Nu**2 + 4*Y/rho))
 
         U2 = primal_dual_TV(Z - D2, 0.01, alpha/rho, 0.001)
@@ -39,7 +47,7 @@ def pidal_tv(Y, K, X, rho, alpha=0.008, max_iter=200):
         U3[U3<0] = 0
 
         # Lagrange's multipliers update
-        D1 = D1 - (K @ Z - U1)
+        D1 = D1 - (K(D, Z) - U1)
         D2 = D2 - (Z - U2)
         D3 = D3 - (Z - U3)
 
